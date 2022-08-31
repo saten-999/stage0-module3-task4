5dc6768e679a409118fb23a2f38edaf5905da0f1
Abrahamyan <asatenik@mgc.mentorg.com>
2022-08-31 17:09:46 +0400

    up

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,10 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+
+        double a=5.6;
+        double b=7.2;
+
+        System.out.println((9 * a * a - 5 * b + 2 + a - 7) * (a + b - 4 * a * b) / 2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c =(int)'c';
+        int h = (int)'h';
+        int a =(int)'a';
+        int r =(int)'r';
+        
+        System.out.println(c);
+        System.out.println(h);
+        System.out.println(a);
+        System.out.println(r);
     }
 }
